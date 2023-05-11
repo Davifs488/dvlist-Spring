6f535b8c7b5f4d3c21be9aa6fe90c0d3b57fc442
Davi Ferreira da Silva <sildv45@gmail.com>
2023-05-11 00:13:25 -0300

    Project created

---
+++ src/main/java/com/aprimorar/executando/ExecutandoApplication.java
@@ -0,0 +1,13 @@
+package com.aprimorar.executando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExecutandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExecutandoApplication.class, args);
+	}
+
+}
