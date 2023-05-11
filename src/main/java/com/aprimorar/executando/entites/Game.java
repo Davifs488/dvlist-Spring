3eabcd40212b9a24c67abe015b7b1a5a9a38ee9b
Davi Ferreira da Silva <sildv45@gmail.com>
2023-05-11 18:24:47 -0300

    Game seed

--- src/main/java/com/aprimorar/executando/entites/Game.java
+++ src/main/java/com/aprimorar/executando/entites/Game.java
@@ -19,24 +19,31 @@ public class Game {
 	@Column(name = "game_year")
 	private Integer year;
 	private String genre;
-	private String platform;
+	private String platforms;
+	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
-	private String LongeDescription;
+	
+	
+	@Column(columnDefinition = "TEXT")
+	private String LongDescription;
 	
 	public Game() {
 			}
 
-	public Game(Long id, String title, Integer year, String genre, String platform, String imgUrl,
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
 			String shortDescription, String longeDescription) {
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.platform = platform;
+		this.platforms = platforms;
+		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
-		LongeDescription = longeDescription;
+		LongDescription = longeDescription;
 	}
 
 	public Long getId() {
@@ -71,12 +78,22 @@ public class Game {
 		this.genre = genre;
 	}
 
-	public String getPlatform() {
-		return platform;
+	public String getPlatforms() {
+		return platforms;
 	}
 
-	public void setPlatform(String platform) {
-		this.platform = platform;
+	public void setPlatforms(String platforms) {
+		this.platforms = platforms;
+	}
+	
+	
+
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
 	}
 
 	public String getImgUrl() {
@@ -96,11 +113,11 @@ public class Game {
 	}
 
 	public String getLongeDescription() {
-		return LongeDescription;
+		return LongDescription;
 	}
 
 	public void setLongeDescription(String longeDescription) {
-		LongeDescription = longeDescription;
+		LongDescription = longeDescription;
 	}
 
 	@Override
