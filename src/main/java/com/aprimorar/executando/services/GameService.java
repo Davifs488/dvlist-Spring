aa94b4a23da00b9c0ba78a564caea03d139f56c6
Davi Ferreira da Silva <sildv45@gmail.com>
2023-05-12 23:15:50 -0300

    GET game by id

--- src/main/java/com/aprimorar/executando/services/GameService.java
+++ src/main/java/com/aprimorar/executando/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.aprimorar.executando.dto.GameDTO;
 import com.aprimorar.executando.dto.GameMinDTO;
 import com.aprimorar.executando.entites.Game;
 import com.aprimorar.executando.repositories.GameRepository;
@@ -16,6 +18,13 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		return new GameDTO(result);
+	}
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findALL(){
 		List<Game>result = gameRepository.findAll();
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
